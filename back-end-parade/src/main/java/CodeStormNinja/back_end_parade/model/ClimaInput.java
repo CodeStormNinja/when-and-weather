c765a67412e577d4b8c75356ead4fe3ab047c27c
Jaiane <jaianesoares700@gmail.com>
2025-10-04 13:27:01 -0300

    feat: ✨️done model ClimaIput

---
+++ back-end-parade/src/main/java/CodeStormNinja/back_end_parade/model/ClimaInput.java
@@ -0,0 +1,40 @@
+package CodeStormNinja.back_end_parade.model;
+package com.CodeStormNinja.back_end_parade.model;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class ClimaInput {
+
+    @JsonProperty("location")
+    private String localidade;
+
+
+    @JsonProperty("datetime")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime dataEHora;
+
+    public ClimaInput(String localidade, LocalDateTime dataEHora) {
+        this.localidade = localidade;
+        this.dataEHora = dataEHora;
+    }
+
+    public String getLocalidade() {
+        return localidade;
+    }
+
+    public void setLocalidade(String localidade) {
+        this.localidade = localidade;
+    }
+
+    public LocalDateTime getDataEHora() {
+        return dataEHora;
+    }
+
+    public void setDataEHora(LocalDateTime dataEHora) {
+        this.dataEHora = dataEHora;
+    }
+}
+}
