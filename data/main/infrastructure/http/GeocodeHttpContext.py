b84cb81c3b5090b92934313b96551f6f5570e157
HenriBDev <henribdev@gmail.com>
2025-10-04 14:37:11 -0300

    ✨ feat - Created initial weather-forecast endpoint

--- data/main/infrastructure/http/GeocodeHttpContext.py
+++ data/main/infrastructure/http/GeocodeHttpContext.py
@@ -1,13 +1,16 @@
 from flask import current_app
 
+from main.infrastructure.http.HttpContext import HttpContext
+
 class GeocodeHttpContext:
 
     def __init__(self):
         self._http_context = HttpContext()
-        self.base_url = current_app.config.get("GEOCODE_API_URL")
+        self._base_url = current_app.config.get("GEOCODE_API_URL")
 
-    def get(self, params):
-        return self._http_context.get(
-            self.base_url,
-            params=params
-        )
+    def get_coordinates_by_location_name(self, location_name):
+        return self._http_context.get(self._base_url + "/search", params={
+            "q": location_name, 
+            "format": "json", 
+            "limit": 1
+        })
--- data/main/infrastructure/http/WeatherForecastHttpContext.py
+++ data/main/infrastructure/http/WeatherForecastHttpContext.py
@@ -1,16 +1,25 @@
-import os
 from flask import current_app
 
-from main.config.HttpConfig import HttpConfig
+from main.infrastructure.http.HttpContext import HttpContext
 
 class WeatherForecastHttpContext:
 
     def __init__(self):
         self._http_context = HttpContext()
-        self.base_url = current_app.config.get("OPEN_METEO_API_URL")
+        self._base_url = current_app.config.get("OPEN_METEO_API_URL") + "/v1"
 
-    def get(self, params):
-        return self._http_context.get(
-            self.base_url,
-            params=params
-        )
+    def forecast_by_coordinates_and_period(self, latitude, longitude, start_date, end_date):
+        return self._http_context.get(self._base_url + "/forecast", params={
+            "latitude": latitude,
+            "longitude": longitude,
+            "hourly": ",".join([
+                "temperature_2m",
+                "rain",
+                "precipitation_probability",
+                "wind_gusts_10m",
+                "snowfall"
+            ]),
+            "start_date": start_date,
+            "end_date": end_date,
+            "timezone": "auto"
+        })
